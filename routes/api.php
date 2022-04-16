ca430a55c9a42cf1a1f915551539b5d25b161fcc
Ismael P. Santana <al222010638@gmail.com>
2022-04-16 01:24:31 -0500

    CRUD Completo

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ Route::prefix('categories')->group(function () {
     Route::get('/', 'Api\CategoriesController@index');
 });
 
+// Payment Methods //
+Route::prefix('payment-methods')->group(function () {
+    Route::get('/', 'Api\PaymentMethodsController@index');
+});
+
+// Products //
+Route::prefix('products')->group(function () {
+    Route::get('/', 'Api\ProductsController@index');
+    Route::get('/{id}', 'Api\ProductsController@show');
+});
+
 // Middleware //
 Route::middleware('auth:sanctum')->group(function () {
     // Auth //
@@ -25,6 +36,27 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/', 'Api\CategoriesController@store');
         Route::put('/{id}', 'Api\CategoriesController@update');
         Route::delete('/{id}', 'Api\CategoriesController@destroy');
+    });
+    // Products //
+    Route::prefix('products')->group(function () {
+        Route::post('/', 'Api\ProductsController@store');
+        Route::put('/{id}', 'Api\ProductsController@update');
+        Route::delete('/{id}', 'Api\ProductsController@destroy');
+    });
+    // Customers //
+    Route::prefix('customers')->group(function () {
+        Route::get('/', 'Api\CustomersController@index');
+        Route::get('/{id}', 'Api\CustomersController@show');
+        Route::post('/', 'Api\CustomersController@store');
+        Route::put('/{id}', 'Api\CustomersController@update');
+        Route::delete('/{id}', 'Api\CustomersController@destroy');
+    });
+    // Sales //
+    Route::prefix('sales')->group(function () {
+        Route::get('/', 'Api\SalesController@index');
+        Route::get('/{id}', 'Api\SalesController@show');
+        Route::post('/', 'Api\SalesController@store');
+        Route::delete('/{id}', 'Api\SalesController@destroy');
     });
 });
 
